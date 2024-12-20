c13e9155a568fc2d2f09a2f4f238fff208c2dac2
Mari Mena <mariafernanda0896@hotmail.com>
2024-12-19 23:12:57 -0300

    input de usuario funcional con máximo 200 carácteres, falta el output del prompt flow y el nombre de la web + logo

--- frontend/pages/prompt.vue
+++ frontend/pages/prompt.vue
@@ -1,9 +1,25 @@
 <template>
-   <textarea
-     class="bg-white text-[#382E2E] placeholder-[#382E2E] p-4 border rounded"
-     placeholder="Escribe tu prompt aquí"
-   ></textarea>
+   <div class="flex flex-col justify-end min-h-screen px-4 pb-8">
+     <div class="w-full flex justify-center">
+       <textarea
+         class="bg-[#F4F4F4] dark:bg-[#0a072b] text-[#382E2E] dark:text-white placeholder-[#B0A8A1] p-4 border-2 border-[#C0C0C0] rounded-lg focus:outline-none w-[1154px] h-[250px] resize-none dark:border-[#0a072b]"
+         placeholder="Escribe aquí..."
+         maxlength="200"
+       ></textarea>
+     </div>
+   </div>
  </template>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  
  <script setup lang="ts">
  </script>
